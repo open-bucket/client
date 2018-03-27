4cd4f4242fb830f0450fc54c9c72b484b5be7d12
vunguyenhung <vunguyenhung@outlook.com>
2018-03-27 23:12:02 +0700

    Add sample component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,8 @@
 // @flow
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from 'antd';
+import styles from './Home.css';
 
 type Props = {};
 
@@ -10,8 +12,9 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div>
-        <div data-tid="container">
+        <div className={styles.container} data-tid="container">
           <h2>Home</h2>
+          <Link to="/counter" data-tid="counter">to Counter</Link>
           <Button type="primary">Test Button with Ant Design</Button>
         </div>
       </div>
