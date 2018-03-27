6bd53b69f90b17e71cec329586d9d33e8a54a213
vunguyenhung <vunguyenhung@outlook.com>
2018-03-27 21:52:50 +0700

    Remove redundant route

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 
 type Props = {};
@@ -13,8 +12,7 @@ export default class Home extends Component<Props> {
       <div>
         <div data-tid="container">
           <h2>Home</h2>
-          <Button type="primary">hehehe</Button>
-          <Link to="/counter">to Counter</Link>
+          <Button type="primary">Test Button with Ant Design</Button>
         </div>
       </div>
     );
