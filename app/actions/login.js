73d6609b44bfd5c4f11746eff23778970db6fbc5
Duy Thanh Nguyen <ndthanhdev@outlook.com>
2018-06-23 15:37:09 +0700

    login

--- app/actions/login.js
+++ app/actions/login.js
@@ -1,16 +1,36 @@
 import { push } from 'react-router-redux';
+import { Auth } from '@open-bucket/daemon';
+import { notification } from 'antd';
 
 export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
 
-export function login() {
+export function login({ username, password }) {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: LOGIN });
+      await Auth.loginP({ username, password });
+      dispatch(loginSuccess());
+    } catch (e) {
+      dispatch(loginFail(e));
+    }
+  };
+}
+
+export function loginSuccess() {
   return (dispatch) => {
-    dispatch({ type: LOGIN });
+    dispatch({ type: LOGIN_SUCCESS });
     dispatch(push('/consumers'));
   };
 }
 
-export function register() {
+export function loginFail(error) {
   return (dispatch) => {
-    dispatch(push('/register'));
+    notification.open({
+      message: 'Could not login',
+      description: error
+    });
+    dispatch({ type: LOGIN_FAIL, error });
   };
 }
--- app/components/Login.js
+++ app/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd';
+import { Form, Icon, Input, Button, Row, Col } from 'antd';
 import styles from './Login.css';
 
 const FormItem = Form.Item;
@@ -8,10 +8,10 @@ class NormalLoginForm extends React.Component {
   handleSubmit = (e) => {
     const { login } = this.props;
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
-        login();
+        login(values);
       }
     });
   }
@@ -27,29 +27,31 @@ class NormalLoginForm extends React.Component {
               <h1>Welcome!</h1>
             </FormItem>
             <FormItem>
-              {getFieldDecorator('userName', {
-                rules: [{ required: false, message: 'Please input your username!' }],
+              {getFieldDecorator('username', {
+                rules: [
+                  { required: true, message: 'Please input your username!' },
+                  { min: 5, message: 'Username must be at least 5 chars long!' }
+                ],
               })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
             </FormItem>
             <FormItem>
               {getFieldDecorator('password', {
-                rules: [{ required: false, message: 'Please input your Password!' }],
+                rules: [
+                  { required: true, message: 'Please input your Password!' },
+                  { min: 5, message: 'Password must be at least 5 chars long!' },
+                ],
               })(<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />)}
             </FormItem>
             <FormItem>
-              {getFieldDecorator('remember', {
-                valuePropName: 'checked',
-                initialValue: true,
-              })(<Checkbox>Remember me</Checkbox>)}
               <Row gutter={8}>
                 <Col span={12}>
                   <Button className={styles.fullWidth} type="ghost" onClick={() => history.push('/register')}>
-                Register Now
+                    Register Now
                   </Button>
                 </Col>
                 <Col span={12}>
                   <Button className={styles.fullWidth} type="primary" htmlType="submit">
-                Log in
+                    Log in
                   </Button>
                 </Col>
               </Row>
